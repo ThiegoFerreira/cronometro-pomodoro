045b0e687389c9134dda4ae2c51d903041d6e1c7
Thiego Ferreira <thiegocpf@gmail.com>
2025-08-24 17:53:02 -0400

    Movendo Container para React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Perferendis adipisci eius ut maxime cumque nulla assumenda nemo aut
-            animi laudantium? Harum voluptate omnis perspiciatis rem aliquid!
-            Corrupti porro error voluptatem!
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
