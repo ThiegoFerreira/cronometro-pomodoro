4061b4fa2dccb759a428ee8f6e7215980ed8aabf
Thiego Ferreira <thiegocpf@gmail.com>
2025-08-24 23:17:47 -0400

    Trabalhando no DefaultInput
    Configurando o componente DefaultInput e setando o tipo de dados aceitos no input

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { Container } from './components/Container';
 import { Menu } from './components/Menu';
 import { Logo } from './components/Logo';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
-
-
 
 
 export function App() {
@@ -19,9 +18,30 @@ export function App() {
       <Container>
         <Menu />
       </Container>
-      
+
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id = 'meuInput' type='text'/>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
