082d756eb5a496ab0721e958010bc34317b783fe
Thiego Ferreira <thiegocpf@gmail.com>
2025-09-03 10:36:10 -0400

    Componente GenericHtml para as páginas que rendereizam HTML
    Implementamos as páginas NotFound(erro 404) e AboutPomodoro(Sobre o pomodoro) reutilizando assim o componente GenericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
   return <Home />;
