c08b3560194c483effba416326efb1880866f5df
Thiego Ferreira <thiegocpf@gmail.com>
2025-09-14 22:16:16 -0400

    TaskContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Home } from './pages/Home';
-import { TaskContextProvider } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 import './styles/theme.css';
 import './styles/global.css';
+
 
 export function App() {
   return (
